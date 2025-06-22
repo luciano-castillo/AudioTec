c28e8d852c624205ebff35cec1c7d875d52ef47a
Raizu548 <rizzoluis53@gmail.com>
2025-06-22 20:28:45 -0300

    Solucion primer electrodomestico

--- AudioTec/Form1.Designer.cs
+++ AudioTec/Form1.Designer.cs
@@ -737,10 +737,11 @@
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "AudioTec";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
